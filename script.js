94266ca5c18527dba0fedfe2af7b2f91c00da755
Micael Rodrigues <micaelr95@outlook.pt>
2018-12-23 23:24:17 +0000

    Display movie query list

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const API_KEY = "cfc9b47942cff33c2255f43dd9fb87f0";
-const API_URL = "https://api.themoviedb.org/3/movie/550?api_key=" + API_KEY + "&";
+const API_SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + "&language=en-US&page=1&include_adult=false&query=";
+const IMAGE_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2"
 
 var cloneMedia = $(".media").clone();
 
@@ -8,21 +9,22 @@ $("#btnSearch").click(function () {
     
     $(".media-list").html("");
 
-    var searchURL = API_URL + "s=" + search;
-    console.log(searchURL);
-
+    var searchURL = API_SEARCH_URL + search;
     $.ajax({
-        "method": "GET",
+        method: "get",
         url: searchURL
     }).done(function(msg){
-        msg.Search.forEach(function (result) {
+        
+        msg.results.forEach(function (result) {
             var liMedia = cloneMedia.clone();
 
-            $(".media-object", liMedia).attr("src", result.backdrop_path);
-            $(".media-heading",liMedia).text(result.Title);
-            $(".ano", liMedia).text(result.Year);
-            $(".tipo", liMedia).text(result.Type);
+            $(".media-object", liMedia).attr("src", IMAGE_URL + result.poster_path);
+            $(".media-heading",liMedia).text(result.title);
+            $(".ano", liMedia).text(result.release_date);
+            $(".tipo", liMedia).text(result.overview);
             $(".media-list").append(liMedia);
         });
     });
+
+    return false;
 });
