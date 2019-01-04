cc58cb128a41149a98af1378c2003996e402bf59
Micael Rodrigues <micaelr95@outlook.pt>
2019-01-04 20:02:44 +0000

    Show popular movies on first page if no search query is provided

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
-const API_KEY = "cfc9b47942cff33c2255f43dd9fb87f0";
-const API_SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + "&language=en-US&page=1&include_adult=false&query=";
-const IMAGE_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2"
+const API_KEY = 'cfc9b47942cff33c2255f43dd9fb87f0';
+const API_BASE_URL = 'https://api.themoviedb.org/3/';
+const API_POPULAR_URL = API_BASE_URL + 'movie/popular?api_key=' + API_KEY;
+const API_SEARCH_URL = API_BASE_URL + 'search/movie?api_key=' + API_KEY + '&language=en-US&page=1&include_adult=false&query=';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2'
 
-var cloneMedia = $(".media").clone();
+let cloneMedia = $('.media').clone();
 
-$("#btnSearch").click(function () {
-    var search = $("#search").val();
-    
-    $(".media-list").html("");
+function media(msg) {
+    msg.results.forEach(function (result) {
+        let liMedia = cloneMedia.clone();
 
-    var searchURL = API_SEARCH_URL + search;
-    $.ajax({
-        method: "get",
-        url: searchURL
-    }).done(function(msg){
-        
-        msg.results.forEach(function (result) {
-            var liMedia = cloneMedia.clone();
-
-            $(".media-object", liMedia).attr("src", IMAGE_URL + result.poster_path);
-            $(".media-heading",liMedia).text(result.title);
-            $(".ano", liMedia).text(result.release_date);
-            $(".tipo", liMedia).text(result.overview);
-            $(".media-list").append(liMedia);
-        });
+        $('.media-object', liMedia).attr('src', IMAGE_URL + result.poster_path);
+        $('.media-heading',liMedia).text(result.title);
+        $('.ano', liMedia).text(result.release_date);
+        $('.tipo', liMedia).text(result.overview);
+        $('.media-list').append(liMedia);
     });
+}
+
+$(function () {
+    $('.media-list').html('');
+
+    console.log(API_POPULAR_URL);
+    $.ajax({
+        method: 'GET',
+        url: API_POPULAR_URL
+    }).done(media);
+});
+
+$('#btnSearch').click(function () {
+    let search = $('#search').val();
+    
+    $('.media-list').html('');
+
+    $.ajax({
+        method: 'GET',
+        url: API_SEARCH_URL + search
+    }).done(media);
 
     return false;
 });
